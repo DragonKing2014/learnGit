f7a313d890002aa4fec9f806cf634f5e9bb7f7ff
Dragon <jlwang_scut@163.com>
2017-04-26 17:32:47 +0800

    commit

--- Web.java
+++ Web.java
@@ -4,6 +4,8 @@ import java.io.*;
 public class Web{
 	public static void main(String[] args){
 		List<Object> objects=new ArrayList<Object>();
+		System.out.println(objects.toString();
+		System.out.println("Hello!");
 		System.out.println("Hello!");
 	}
 }
