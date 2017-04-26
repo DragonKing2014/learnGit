0956665879fa806f8ff6189e5372ec11dfa5420a
Dragon <jlwang_scut@163.com>
2017-04-26 17:30:56 +0800

    git java

--- Web.java
+++ Web.java
@@ -1,8 +1,9 @@
-package com
+package com;
+import java.io.*;
 
-import java.io.*
 public class Web{
 	public static void main(String[] args){
+		List<Object> objects=new ArrayList<Object>();
 		System.out.println("Hello!");
 	}
 }
